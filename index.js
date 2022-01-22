3167be26b7ff6a27ded4dc889d7be87125a13c1d
TheNootLinja <thenootlinja@gmail.com>
2022-01-22 16:21:05 -0500

    Add code for finished project

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const people = [
   { name: 'Lux', year: 2015 },
 ];
 
-const comments = [
+let comments = [
   { text: 'Love this!', id: 523423 },
   { text: 'Super good', id: 823423 },
   { text: 'You are the best', id: 2039842 },
@@ -19,12 +19,23 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+const olderThanNineteen = (person) => 2022 - person.year >= 19;
+console.log(`One person over 19: ${people.some(olderThanNineteen)}`);
 // Array.prototype.every() // is everyone 19 or older?
-
+console.log(`Everyone over 19: ${people.every(olderThanNineteen)}`);
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
-
+const selectedComment = comments.find((comment) => comment.id === 823423);
+console.log(`Comment with id of 823423: ${selectedComment.text}`);
 // Array.prototype.findIndex()
 // Find the comment with this ID
+const selectedCommentIndex = comments.findIndex(
+  (comment) => comment.id === 823423
+);
 // delete the comment with the ID of 823423
+// Using filter
+const commentRemoved = comments.filter((value, index, comments) => {
+  return index !== selectedCommentIndex;
+});
+console.log(commentRemoved);
